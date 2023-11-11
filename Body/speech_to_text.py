ebfba7e16c1b7e06afdf8865f04c507337e28a7e
Vinayak M <vinayakmahajan06@gmail.com>
2023-11-12 02:01:13 +0530

    STT

---
+++ Body/speech_to_text.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+
+import pyttsx3
+
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Listening...")
+
+        try:
+            audio = recognizer.listen(source, timeout=5)
+            print("Recognizing...")
+
+            text = recognizer.recognize_google(audio)
+            # print(f"You said: {text}")
+            return text
+
+        except sr.UnknownValueError:
+            print("Could not understand the audio.")
+        except sr.RequestError:
+            print("Could not request results. Check your internet connection.")
+
+while True:
+    speech_text = speech_to_text()
+
+    with open('AI_Brains\cookies\transcribe.txt', 'w') as file:
+        file.write(speech_text)
+        # print(user_input)
+        # if user_input:
+        #     text_to_speech(user_input)
