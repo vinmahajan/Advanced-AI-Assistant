0ef996de73446aef450978c2a23f578f02de66e9
Vinayak M <vinayakmahajan06@gmail.com>
2024-12-01 15:11:34 +0530

    updated

--- AI_Brains/Llama.py
+++ AI_Brains/Llama.py
@@ -3,88 +3,90 @@ import json
 from groq import Groq
 from datetime import datetime
 from dotenv import load_dotenv
+# from googlesearch import search
+from Body import Internet_Data
 
 load_dotenv() 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-history_path = "AI_Brains/History/"
-date= datetime.now().date()
-
-
+date = datetime.now().date()
+history_path = f"AI_Brains/History/{date}.txt"
 
 def getHistory():
+    # history_file = f"{history_path}{date}.txt"
     try:
-        with open(history_path+str(date)+".txt", "r") as file:
-            history = file.read()
-    except:
-        history=""
+        with open(history_path, "r") as file:
+            return file.read()
+    except FileNotFoundError: 
+        return ""  
 
-    return history
+with open("AI_Brains/AI_System_Instructions.txt", "r") as f:
+    instructions = f.read()
 
-
-
-
-def Ask_llama(query, AI_NAME):
-    instruction = "Imagine you are an AI named \""+AI_NAME+"\" that responds like a human. Keep responses brief and always in JSON format. Use the key \""+AI_NAME+"\" for your response, and if tasks are assigned, label them as \"Task1\", \"Task2\", and so on. Each task should include a sub-dictionary with \"Action\" for the task and \"ActionValue\" for its value. \nExample:\nUser command: \"turn on the fan1 and set the speed to 4\"\nYour response: { \""+AI_NAME+"\": \"Ok, turning on the fan and setting speed to 4\", \"Task1\": {\"Action\": \"on_fan1\", \"ActionValue\": \"4\"}}"
+def Ask_LLM(query, SearchResult = None, AddToHistory = True):
+    print(f"\n\033[93m>> Making Query request to LLM: '{query}'...\033[0m", end=" ")
     history = getHistory()
+    if SearchResult != None:
+        llama_query = f"""Previous Conversation: {history}\n```\n Search Results: {SearchResult}\n```\n Today's date: {str(date)}\n```\n Query: {query}"""
+    else:
+        # llama_query = "History: " + history + "\n``Today's date: "+ str(date)+"\nquery: " + query 
+        llama_query = f"""Previous Conversation: {history}\n```\n Today's date: {str(date)}\n```\n Query: {query}"""
+
+
     client = Groq(api_key=GROQ_API_KEY)
     completion = client.chat.completions.create(
         model="llama-3.1-70b-versatile",
-        messages=[{
-                "role": "system",
-                "content": instruction
-            },
-            {
-                "role": "user",
-                "content":"History: "+history+"\nuser query"+ query
-            }
-
-            ],
+        messages=[
+            {"role": "system", "content": instructions},
+            {"role": "user", "content":llama_query}
+        ],
         temperature=1,
         max_tokens=1024,
         top_p=1,
         stream=True,
         stop=None,
     )
-    response = ""
-    for chunk in completion:
-        response += chunk.choices[0].delta.content or ""
+
+    response = ''.join(chunk.choices[0].delta.content or "" for chunk in completion)
+    # print(f"String response: {response}")
     # Parse the JSON string
     parsed_json = json.loads(response)
 
+    # if parsed_json.get("Task2", {}).get("Action") == "RealTimeData":
+    #     AddToHistory = False
+
     # save the conversation to history file
-    context ="{\"role\":\"user\",\"content\":\"" + query + "\"}\n{\"role\":\"model\",\"content\":\"" + str(parsed_json) + "\"},\n"
-
-    
-    with open(history_path+str(date)+".txt", "a") as file:
-        file.write(context)
-
+    if AddToHistory:
+        context = (
+        f'{{"role":"user","content":"{query}"}}\n'
+        f'{{"role":"model","content":{parsed_json}}},\n'
+    )
+        with open(history_path, "a") as file:
+            file.write(context)
+    print(f"Done.")
+    print(f"\033[92m>>LLM: {parsed_json}\033[0m\n")
     return parsed_json
 
 
 
+def get_llama(query):
+    parsed_json = Ask_LLM(query, SearchResult = None, AddToHistory = True)
+    
+    task1 = parsed_json.get("Task1", {})
+    task1_action = task1.get("Action")
+    task1_action_value = task1.get("ActionValue")
 
-# response = Ask_llama(text="what is your name", AI_NAME="mini")
-# response
+    task2 = parsed_json.get("Task2", {})
+    task2_action = task2.get("Action")
+    task2_action_value = task2.get("ActionValue")
 
 
-# def template(query, AI_NAME):
-#     os.makedirs(os.path.dirname(history_path), exist_ok=True)
-#     try:
-#         with open(history_path+str(date)+".txt", "r") as file:
-#             text = json.loads("["+file.read().replace("\n", "").strip(",")+"]")
-#     except:
-#         text=[]
-
-#     instruction = "Imagine you are an AI named \""+AI_NAME+"\" that responds like a human. Keep responses brief and always in JSON format. Use the key \""+AI_NAME+"\" for your response, and if tasks are assigned, label them as \"Task1\", \"Task2\", and so on. Each task should include a sub-dictionary with \"Action\" for the task and \"ActionValue\" for its value. \nExample:\nUser command: \"turn on the fan1 and set the speed to 4\"\nYour response: { \""+AI_NAME+"\": \"Ok, turning on the fan and setting speed to 4\", \"Task1\": {\"Action\": \"on_fan1\", \"ActionValue\": \"4\"}}"
-#     template=[{
-#                 "role": "system",
-#                 "content": instruction
-#             }]
-#     template.extend(text)
-#     template.extend([{
-#                     "role": "user",
-#                     "content": query
-#                 }])
-
-#     return template
+    if task2_action == "RealTimeData" and task2_action_value:
+        print(f">> Collecting Data for: '{task2_action_value}'... ", end="")
+        internet_data = Internet_Data.get_internet_data(task2_action_value)
+        if internet_data:
+            print(f"Done.\n>>>>Data: {internet_data}")
+            return Ask_LLM(query=query, SearchResult=internet_data)
+        else:
+            print("Failed to get internet data.")
+    return parsed_json
